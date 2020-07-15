eae674d56266cbd1ee2597a58f2bfd2f69660c62
zhangjunyang <2816886869@qq.com>
2020-07-15 21:16:45 +0800

    “sign”

--- js/index.js
+++ js/index.js
@@ -550,8 +550,6 @@ function echarts_4() {
         var myChart = echarts.init(document.getElementById('echart4'));
         var dayDatas=[
 
-     
-      {"date":"20","money":86.81,"rate":87},//14
       {"date":"21","money":51.72,"rate":52},//14
       {"date":"22","money":70.42,"rate":70},//15
       {"date":"23","money":72.92,"rate":72},//7
@@ -575,6 +573,7 @@ function echarts_4() {
       {"date":"11","money":64.65,"rate":65},//6
       {"date":"12","money":70.05,"rate":70},//11
       {"date":"13","money":71.42,"rate":71},//10
+      {"date":"14","money":71.05,"rate":71},//14
       ];
 
         var dates=[];
